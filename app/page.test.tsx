064cf5fbbae09c5d1ae7a0f49b5743a6216c62c3
terenlam <168493225+terenlam@users.noreply.github.com>
2026-09-01 20:09:55 -0400

    Initial commit

---
+++ app/page.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import Page from "../app/page";
+
+test("Page", () => {
+  render(<Page />);
+  expect(screen.getByRole("heading", { level: 1, name: "Home" })).toBeDefined();
+});
