d5e67a56b1b9273e66feccb405760d59b0710d52
Terence Lam <terencelammail@gmail.com>
2026-09-03 15:51:21 -0400

    feat: add first level heading titled "Medvim"

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
     <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
+      <h1>Medvim</h1>
     </div>
   );
 }
