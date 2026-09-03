2b4bde1db302d6510bb4e4c303f2ca95f0bbc91b
Terence Lam <terencelammail@gmail.com>
2026-09-03 15:49:40 -0400

    test: add a test to ensure that Page renders a heading titled "Medvim"

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,7 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import Page from "../app/page";
 
-test("Page", () => {
-  render(<Page />);
-  expect(screen.getByRole("heading", { level: 1, name: "Home" })).toBeDefined();
+describe("Page", () => {
+  it("renders a first level heading titled 'Medvim'", () => {
+    render(<Page />);
+    expect(screen.getByRole("heading", { level: 1, name: "Medvim" })).toBeDefined();
+  });
 });
