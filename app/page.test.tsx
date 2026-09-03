7282243d3211a441b25dde2b20102b5d6bec34c3
Terence Lam <terencelammail@gmail.com>
2026-09-03 17:02:14 -0400

    test: edit page.test.tsx to ensure that Page renders the same snapshot

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Page from "../app/page";
 
 describe("Page", () => {
   it("renders a first level heading titled 'Medvim'", () => {
-    render(<Page />);
-    expect(screen.getByRole("heading", { level: 1, name: "Medvim" })).toBeDefined();
+    const page = render(<Page />);
+    expect(page).toMatchSnapshot();
   });
 });
