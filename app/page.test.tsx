7d6ef79b78aef42a1a021eb3868867947eba11da
Terence Lam <terencelammail@gmail.com>
2026-09-04 17:15:38 -0400

    test: remove page.test.tsx and its snapshot

--- app/page.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Page from "../app/page";
-
-describe("Page", () => {
-  it("renders a first level heading titled 'Medvim'", () => {
-    const page = render(<Page />);
-    expect(page).toMatchSnapshot();
-  });
-});
